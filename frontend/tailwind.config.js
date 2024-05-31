4e6e7e5d4eada004ccd24b003bd1c19848d18b93
UsamaAsif1212 <usamaasif670@gmail.com>
2024-05-31 16:32:26 +0500

    video

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -20,5 +21,29 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+   function ({ addUtilities }) {
+      const newUtilities = {
+        '.scrollbar-thin': {
+          scrollbarWidth : "thin",
+          scrollbarColor : "rgb(31 29 29) white",
+          },
+          ".scrollbar-webkit":{
+            "&::-webkit-scrollbar" :{
+              width: "8px"
+            },
+            "&::-webkit-scrollbar-track":{
+                background: "white"
+              },
+              "&::-webkit-scrollbar-thumb":{
+                backgroundColor: "rgb(31 41 55)",
+                borderRadius: "20px",
+                border: "1px solid white",
+              },
+          }
+        }
+
+        addUtilities(newUtilities, ["responsive", "hover"])
+      }
+  ],
 }
